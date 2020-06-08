8bccbd333ec2343e81f8d3470ecbe3afa3e09ce0
ikbal512 <iqbal@raharja.info>
2020-06-08 16:40:08 +0700

    Add Votes, Answers, and Views Counters to Question item

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,12 +14,9 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        \DB::enableQueryLog();
         $questions = Question::with('user')->latest()->paginate(5);
 
-        view('questions.index', compact('questions'))->render();
-
-        dd(\DB::getQueryLog());
+        return view('questions.index', compact('questions'));
     }
 
     /**
