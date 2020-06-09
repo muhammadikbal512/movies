cdd6e8ac52bcbe8222ee7f27d416120c541c9170
ikbal512 <iqbal@raharja.info>
2020-06-09 20:32:31 +0700

    fix counters

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -18,15 +18,25 @@
                     @include('layouts._messages')
                     @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes) }}
+                                </div>
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong> {{ Str::plural('answer', $question->answers) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views . " " . Str::plural('view', $question->views) }}
+                            </div>
+                            </div>
                             <div class="media-body">
                                 <div class="d-flex align-items-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                        @if(Auth::user()->can('update-question', $question))
+                                        @if (Auth::user()->can('update-question', $question))
                                             <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endif
-
-                                        @if(Auth::user()->can('delete-question', $question))
+                                        @if (Auth::user()->can('delete-question', $question))
                                             <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
                                                 @method('DELETE')
                                                 @csrf
