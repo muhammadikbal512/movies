7b89f0c848d04889890b61d12548db491e26cfed
ikbal512 <iqbal@raharja.info>
2020-06-10 15:48:01 +0700

    designing answer table schema and rename answers to answers_count

--- app/User.php
+++ app/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
         // return route('questions.show', $this->id);
         return '#';
     }
+
+    public function answers()
+    {
+        $this->hasMany(Answer::class);
+    }
 }
