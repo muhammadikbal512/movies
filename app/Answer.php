8a9ef9ecdac29e47cd14e60493efbe3ed347f6ad
ikbal512 <iqbal@raharja.info>
2020-06-10 16:52:50 +0700

    display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -27,7 +27,9 @@ class Answer extends Model
             $answer->question->increment('answers_count');
             $answer->question->save();
         });
-
-        
+    }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
