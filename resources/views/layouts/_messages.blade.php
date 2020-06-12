2bbe7b3b1da3a68c7d10b6c94bc53168deab911e
ikbal512 <iqbal@raharja.info>
2020-06-12 14:38:29 +0700

    delete the answers

--- resources/views/layouts/_messages.blade.php
+++ resources/views/layouts/_messages.blade.php
@@ -42,3 +42,12 @@
         </button>
     </div>
 @endif
+
+@if (session('deleteAnswer'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> Your answer has been deleted! {{ session('sucess') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
