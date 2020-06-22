aff69742fcbb154303ea5f6a0d34428c86d600dd
ikbal512 <iqbal@raharja.info>
2020-06-22 21:03:51 +0700

    rewrite the vote control into vue.js component

--- app/User.php
+++ app/User.php
@@ -80,14 +80,14 @@ class User extends Authenticatable
     {
         $voteQuestions = $this->voteQuestions();
         
-        $this->_vote($voteQuestions, $question, $vote);
+        return $this->_vote($voteQuestions, $question, $vote);
     }
 
     public function voteAnswer(Answer $answer, $vote)
     {
         $voteAnswers = $this->voteAnswers();
         
-        $this->_vote($voteAnswers, $answer, $vote);
+        return $this->_vote($voteAnswers, $answer, $vote);
     }
 
     private function _vote($relationship, $model, $vote)
@@ -105,5 +105,7 @@ class User extends Authenticatable
         
         $model->votes_count = $upVotes + $downVotes;
         $model->save();
+
+        return $model->votes_count;
     }
 }
