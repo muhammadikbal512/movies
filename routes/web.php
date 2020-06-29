64b1b9d24e0cf51f6ae6cfe0a89f8f1b32b1375a
ikbal512 <iqbal@raharja.info>
2020-06-29 14:37:08 +0700

    Create api endpoints for favoriting the question & accepting answer

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,10 @@ Route::resource('questions', 'QuestionsController')->except('show');
 // Route::post('/questions{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::resource('questions.answers', 'AnswersController')->except(['create', 'show']);
 
-Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
-
 
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
 
+Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
 Route::post('/question/{question}/favorites', 'FavoritesController@store')->name('questions.favorite');
 Route::delete('/question/{question}/favorites', 'FavoritesController@destroy')->name('questions.unfavorite');
 
